e3911f47f1ca775ed7bb2c1d67a7fe96397a0221
OrKoN <alexei.rudenko@gmail.com>
2017-02-03 21:14:24 +0100

    Migration to new base-x-rs version

--- native/src/lib.rs
+++ native/src/lib.rs
@@ -11,15 +11,15 @@ use neon::mem::Handle;
 fn encode(call: Call) -> JsResult<JsString> {
   let scope = call.scope;
   let alphabet: Handle<JsString> = try!(try!(call.arguments.require(scope, 0)).check::<JsString>());
-  let alphabet =  &alphabet.value();
+  let alphabet: &str = &alphabet.value()[..];
   let buffer: Handle<JsBuffer> = try!(try!(call.arguments.require(scope, 1)).check::<JsBuffer>());
   let result = vm::lock(buffer, |data| {
-    let mut vec:Vec<i16> = Vec::with_capacity(data.len());
+    let mut vec:Vec<u8> = Vec::with_capacity(data.len());
     let pointer = data.as_ptr();
     for i in 0..data.len() {
-      vec.push(unsafe { *pointer.offset(i as isize) as i16 });
+      vec.push(unsafe { *pointer.offset(i as isize) as u8 });
     }
-    base_x::encode(alphabet, vec)
+    base_x::encode(alphabet, &vec)
   });
   Ok(JsString::new(scope, &result).unwrap())
 }
@@ -27,10 +27,10 @@ fn encode(call: Call) -> JsResult<JsString> {
 fn decode(call: Call) -> JsResult<JsBuffer> {
   let scope = call.scope;
   let alphabet: Handle<JsString> = try!(try!(call.arguments.require(scope, 0)).check::<JsString>());
-  let alphabet =  &alphabet.value();
+  let alphabet: &str = &alphabet.value()[..];
   let input: Handle<JsString> = try!(try!(call.arguments.require(scope, 1)).check::<JsString>());
   let input =  &input.value();
-  let result = base_x::decode(alphabet, input);
+  let result = base_x::decode(alphabet, input).unwrap();
   let buffer = try!(JsBuffer::new(scope, result.len() as u32));
   vm::lock(buffer, |data| {
     let mut b = data;
