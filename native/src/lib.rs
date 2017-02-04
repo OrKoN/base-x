786ac8fd94e1b29fd956329b0e6c83a2d5e610ec
Friedel Ziegelmayer <dignifiedquire@gmail.com>
2017-02-04 22:39:12 +0100

    update rust dependencies

--- native/src/lib.rs
+++ native/src/lib.rs
@@ -3,48 +3,51 @@ extern crate neon;
 extern crate base_x;
 
 use neon::vm;
-use neon::vm::{Call, JsResult};
-use neon::js::{JsString};
+use neon::vm::{Call, JsResult, Lock};
+use neon::js::JsString;
 use neon::js::binary::JsBuffer;
 use neon::mem::Handle;
 
 fn encode(call: Call) -> JsResult<JsString> {
-  let scope = call.scope;
-  let alphabet: Handle<JsString> = try!(try!(call.arguments.require(scope, 0)).check::<JsString>());
-  let alphabet: &str = &alphabet.value()[..];
-  let buffer: Handle<JsBuffer> = try!(try!(call.arguments.require(scope, 1)).check::<JsBuffer>());
-  let result = vm::lock(buffer, |data| {
-    let mut vec:Vec<u8> = Vec::with_capacity(data.len());
-    let pointer = data.as_ptr();
-    for i in 0..data.len() {
-      vec.push(unsafe { *pointer.offset(i as isize) as u8 });
-    }
-    base_x::encode(alphabet, &vec)
-  });
-  Ok(JsString::new(scope, &result).unwrap())
+    let scope = call.scope;
+    let alphabet: Handle<JsString> = try!(try!(call.arguments.require(scope, 0))
+        .check::<JsString>());
+    let alphabet: &str = &alphabet.value()[..];
+    let mut buffer: Handle<JsBuffer> = try!(try!(call.arguments.require(scope, 1))
+        .check::<JsBuffer>());
+    let result = buffer.grab(|data| {
+        let mut vec: Vec<u8> = Vec::with_capacity(data.len());
+        let pointer = data.as_ptr();
+        for i in 0..data.len() {
+            vec.push(unsafe { *pointer.offset(i as isize) as u8 });
+        }
+        base_x::encode(alphabet, &vec)
+    });
+    Ok(JsString::new(scope, &result).unwrap())
 }
 
 fn decode(call: Call) -> JsResult<JsBuffer> {
-  let scope = call.scope;
-  let alphabet: Handle<JsString> = try!(try!(call.arguments.require(scope, 0)).check::<JsString>());
-  let alphabet: &str = &alphabet.value()[..];
-  let input: Handle<JsString> = try!(try!(call.arguments.require(scope, 1)).check::<JsString>());
-  let input =  &input.value();
-  let result = base_x::decode(alphabet, input).unwrap();
-  let buffer = try!(JsBuffer::new(scope, result.len() as u32));
-  vm::lock(buffer, |data| {
-    let mut b = data;
-    let mut i = 0;
-    for x in & result {
-      b[i] = *x as u8;
-      i=i+1;
-    }
-  });
-  Ok(buffer)
+    let scope = call.scope;
+    let alphabet: Handle<JsString> = try!(try!(call.arguments.require(scope, 0))
+        .check::<JsString>());
+    let alphabet: &str = &alphabet.value()[..];
+    let input: Handle<JsString> = try!(try!(call.arguments.require(scope, 1)).check::<JsString>());
+    let input = &input.value();
+    let result = base_x::decode(alphabet, input).unwrap();
+    let mut buffer = try!(JsBuffer::new(scope, result.len() as u32));
+    buffer.grab(|mut data| {
+        // let mut b = data;
+        let mut i = 0;
+        for x in &result {
+            data[i] = *x as u8;
+            i = i + 1;
+        }
+    });
+    Ok(buffer)
 }
 
 register_module!(m, {
-  try!(m.export("encode", encode));
-  try!(m.export("decode", decode));
-  Ok(())
+    try!(m.export("encode", encode));
+    try!(m.export("decode", decode));
+    Ok(())
 });
